a70a6e45e4cbd8e8e3ca4e32cd4c55f66fc0ca73
enzoesper <enzoesper@hotmail.com>
2020-12-11 17:08:50 -0300

    Creating a Callback compoent to parse the callback URL with the acces and id token

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
 import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 class App extends React.Component {
   
   constructor(props) {
@@ -20,6 +21,7 @@ class App extends React.Component {
         <div className="body">
           {/* how to pass the Auth object as a prop to a component since the component itself is delcared as a prop? the solution is use a render prop instead*/}
           <Route path="/" exact render={props => <Home auth={this.auth} {...props} />}/>
+          <Route path="/callback" render={props => <Callback auth={this.auth} {...props} />}/>
           <Route path="/profile" component={Profile}/>
         </div>
       </>
---
+++ src/Callback.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Callback extends Component {
+
+  componentDidMount() {
+    // Handle authentication if expected values are in the url
+    if (/acces_token|id_token|error/.test(this.props.location.hash)) {
+      this.props.auth.handleAuthentication();
+    } else {
+      throw new Error("Invalid callback URL");
+    }
+  }
+
+  render() {
+    return (
+      <h1>
+        Loading...
+      </h1>
+    );
+  }
+}
+
+export default Callback;
