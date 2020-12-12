7abb0c49a815629109984c65d57717089211a497
enzoesper <enzoesper@hotmail.com>
2020-12-12 00:53:08 -0300

    fixing the url for the public endpoint

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.get('public', (req, res) => {
+app.get('/public', (req, res) => {
   res.json({
     message: "Hello from a public API endpoint"
   });
