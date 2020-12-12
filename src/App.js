8801af36cf96a96198b11406df20c354b1133d36
enzoesper <enzoesper@hotmail.com>
2020-12-11 23:52:04 -0300

    configuring the profile page route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
@@ -22,7 +22,7 @@ class App extends React.Component {
           {/* how to pass the Auth object as a prop to a component since the component itself is delcared as a prop? the solution is use a render prop instead*/}
           <Route path="/" exact render={props => <Home auth={this.auth} {...props} />}/>
           <Route path="/callback" render={props => <Callback auth={this.auth} {...props} />}/>
-          <Route path="/profile" component={Profile}/>
+          <Route path="/profile" render={props => this.auth.isAuthenticated() ? <Profile auth={this.auth} {...props} /> : <Redirect to="/" />}/>
         </div>
       </>
     );
