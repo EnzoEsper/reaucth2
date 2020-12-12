14daefb2d187ee1ca1458118caebbbe233c30f7d
enzoesper <enzoesper@hotmail.com>
2020-12-12 03:09:47 -0300

    calling the private API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Nav from './Nav';
 import Auth from "./Auth/Auth";
 import Callback from "./Callback";
 import Public from "./Public";
+import Private from "./Private";
 class App extends React.Component {
   
   constructor(props) {
@@ -25,6 +26,7 @@ class App extends React.Component {
           <Route path="/callback" render={props => <Callback auth={this.auth} {...props} />}/>
           <Route path="/profile" render={props => this.auth.isAuthenticated() ? <Profile auth={this.auth} {...props} /> : <Redirect to="/" />}/>
           <Route path="/public" component={Public}/>
+          <Route path="/private" render={props => this.auth.isAuthenticated() ? <Private auth={this.auth} {...props}/> : this.auth.login()}/>
         </div>
       </>
     );
