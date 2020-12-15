e98dd22ce5229409013dd4e099b55b5ab6c53f9f
enzoesper <enzoesper@hotmail.com>
2020-12-15 19:26:42 -0300

    adding acces to an endpoint based on a certain role (check role functionallity)

--- server.js
+++ server.js
@@ -45,6 +45,24 @@ app.get('/courses', checkJwt, checkScope(["read:courses"]), (req, res) => {
   });
 });
 
+function checkRole(role) {
+  return function(req, res, next) {
+    const assignedRoles = req.user["http://localhost:3000/roles"];
+    if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+      return next();
+    } else {
+      return res.status(401).send("Insufficient role");
+    }
+  };
+}
+
+// admin only enpoint
+app.get('/admin', checkJwt, checkRole('admin'), (req, res) => {
+  res.json({
+    message: "Hello from an ADMIN API endpoint"
+  });
+});
+
 app.listen(3001, () => {
   console.log("API server listening on", process.env.REACT_APP_API_URL);
 });
